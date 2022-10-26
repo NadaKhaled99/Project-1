304b7447e99b62053175effa91ceaeb7adf72fd3
Nada Khaled <100424417+NadaKhaled99@users.noreply.github.com>
2022-10-26 22:22:57 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,118 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <string>
+using namespace std;
+
+ 
+ 
+ 
+int
+main ()
+{
+ 
+ 
+ 
+ 
+char ch;
+ 
+ 
+ 
+ 
+cin >> ch;
+ 
+ 
+ 
+ 
+switch (ch)
+   
+ 
+ 
+ 
+    {
+   
+ 
+ 
+ 
+case
+'a':
+   
+ 
+ 
+case
+'e':
+   
+ 
+ 
+case
+'i':
+   
+ 
+ 
+case
+'o':
+   
+ 
+ 
+case
+'u':
+   
+ 
+ 
+case
+'A':
+   
+ 
+ 
+case
+'E':
+   
+ 
+ 
+case
+'I':
+   
+ 
+ 
+case
+'O':
+   
+ 
+ 
+case
+'U':
+     
+ 
+ 
+cout << ch << " is a vowel";
+     
+ 
+ 
+ 
+break;
+   
+default:
+     
+ 
+ 
+ 
+cout << ch << " is not a vowel";
+   
+ 
+ 
+ 
+}
+ 
+ 
+ 
+return 0;
+
+ 
+ 
+}
